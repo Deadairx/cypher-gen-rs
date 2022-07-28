cd702f5294b48d2a7e7f4a7e7354230d2903b4c2
DeadAirX <djdivinity7@gmail.com>
2022-07-28 09:09:11 -0500

    add cargo run to autotest

--- autotest.lua
+++ autotest.lua
@@ -1,25 +1,42 @@
-local bufnr = 8
+local bufnr = 9
 
+function cargo_test ()
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "=====TESTS=====" })
+    vim.fn.jobstart({"cargo", "test"}, {
+        stdout_buffered = true,
+        on_stdout = function(_, data)
+            if data then
+                vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
+            end
+        end,
+        on_stderr = function(_, data)
+            if data then
+                vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
+            end
+        end,
+    })
+end
+
+function cargo_run ()
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "+++++RUN+++++" })
+    vim.fn.jobstart({"cargo", "run", "--", "-s", ", ", "-p", "zyxwvutsrqponmlkjihgfedcba", "hello world"}, {
+        stdout_buffered = true,
+        on_stdout = function(_, data)
+            if data then
+                vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
+            end
+        end,
+        on_stderr = function(_, data)
+            if data then
+                vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
+            end
+        end,
+    })
+end
 
 vim.api.nvim_create_autocmd("BufWritePost", {
     group = vim.api.nvim_create_augroup("autotest", { clear = true }),
     pattern = "main.rs",
-    callback = function()
-        vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { "=====TESTS=====" })
-        vim.fn.jobstart({"cargo", "test"}, {
-            stdout_buffered = true,
-            on_stdout = function(_, data)
-                if data then
-                    vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
-                end
-            end,
-            on_stderr = function(_, data)
-                if data then
-                    vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
-                end
-            end,
-
-        })
-    end,
+    callback = cargo_run,
 })
 
